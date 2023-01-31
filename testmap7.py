0b18aa6bfaf535b6858c341b0dd15a2ccc3722c5
kingfisher <124000097+Ssurf777@users.noreply.github.com>
2023-01-31 22:37:59 +0900

    Update testmap7.py

--- testmap7.py
+++ testmap7.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import folium
-from streamlit_folium import folium_static
+#from streamlit_folium import folium_static
 import pandas as pd
 
 sales_office = pd.DataFrame(
