db6d313898632dd0fdae2f4f7d80c2b3e600db14
kingfisher <124000097+Ssurf777@users.noreply.github.com>
2023-02-01 23:11:28 +0900

    Update map7.py

--- map7.py
+++ map7.py
@@ -80,9 +80,11 @@ with st.sidebar:
 
     if workday=='Yes':
         df4=df3[(df3["weekday"] >=2) & (df3["weekday"] <=6)]
+        cdf=df2[(df2["weekday"] >=2) & (df2["weekday"] <=6)]
     else:
         df4=df3[(df3["weekday"] ==1) | (df3["weekday"] ==7)]
-    #df4
+        cdf=df2[(df2["weekday"] ==1) | (df2["weekday"] ==7)]
+    #cdf
 #-------------------------------------------------------------
     daynight=st.selectbox(
 	'select day or night',
@@ -90,8 +92,10 @@ with st.sidebar:
 
     if daynight=='day':
         df5=df4[(df4["daynight"] <20)]
+        cdf=cdf[(cdf["daynight"] <20)]
     else:
         df5=df4[(df4["daynight"] >=20)]
+        cdf=cdf[(cdf["daynight"] >=20)]
     #df5
 #-------------------------------------------------------------
     weather=st.selectbox(
@@ -100,10 +104,13 @@ with st.sidebar:
 
     if weather=='sunny&cloudy':
         df6=df5[(df5["weather"] ==1) | (df5["weather"] ==2)]
+        cdf=cdf[(cdf["weather"] ==1) | (cdf["weather"] ==2)]
     if weather=='rain':
         df6=df5[(df5["weather"] ==3)]
+        cdf=cdf[(cdf["weather"] ==3)]
     if weather=='snow':
         df6=df5[(df5["weather"] ==5)]
+        cdf=cdf[(cdf["weather"] ==5)]
     #df6
 #-------------------------------------------------------------
     type=st.selectbox(
@@ -111,10 +118,13 @@ with st.sidebar:
 	('vechicle<>vechicle','person<>vechicle','vechicle_only'))
     if type=='vechicle<>vechicle':
         df7=df6[(df6["cont"] ==21)]
+        cdf=cdf[(cdf["cont"] ==21)]
     if type=='vechicle_only':
         df7=df6[(df6["cont"] ==41)]
+        cdf=cdf[(cdf["cont"] ==41)]        
     if type=='person<>vechicle':
         df7=df6[(df6["cont"] ==1)]
+        cdf=cdf[(cdf["cont"] ==1)]
     #df7
 
 #-------------------------------------------------------------
@@ -122,6 +132,43 @@ with st.sidebar:
     df8=df7.loc[:,['x','y']]
     st.write("Counter",df8["x"].count())
 
+#st.write("Count 1",cdf[cdf["month"]==1].count().min())
+#st.write("Count 2",cdf[cdf["month"]==2].count().min())
+#st.write("Count 3",cdf[cdf["month"]==3].count().min())
+#st.write("Count 4",cdf[cdf["month"]==4].count().min())
+#st.write("Count 5",cdf[cdf["month"]==5].count().min())
+#st.write("Count 6",cdf[cdf["month"]==6].count().min())
+#st.write("Count 7",cdf[cdf["month"]==7].count().min())
+#st.write("Count 8",cdf[cdf["month"]==8].count().min())
+#st.write("Count 9",cdf[cdf["month"]==9].count().min())
+#st.write("Count 10",cdf[cdf["month"]==10].count().min())
+#st.write("Count 11",cdf[cdf["month"]==11].count().min())
+#st.write("Count 12",cdf[cdf["month"]==12].count().min())
+#st.write(cdf.describe())
+
+ndarray = np.array([[0, 0],
+                    [1, cdf[cdf["month"]==1].count().min()],
+                    [2, cdf[cdf["month"]==2].count().min()],
+                    [3, cdf[cdf["month"]==3].count().min()],
+                    [4, cdf[cdf["month"]==4].count().min()],
+                    [5, cdf[cdf["month"]==5].count().min()],
+                    [6, cdf[cdf["month"]==6].count().min()],
+                    [7, cdf[cdf["month"]==7].count().min()],
+                    [8, cdf[cdf["month"]==8].count().min()],
+                    [9, cdf[cdf["month"]==9].count().min()],
+                    [10, cdf[cdf["month"]==10].count().min()],
+                    [11, cdf[cdf["month"]==11].count().min()],
+                    [12, cdf[cdf["month"]==12].count().min()],
+                   ])
+summary=pd.DataFrame(ndarray, columns=["month", "count"])
+#summary.loc[:,["month"]]
+#st.line_chart(summary)
+st.header("number of accidents")
+st.write("settings",daynight)
+st.write("settings",weather)
+st.write("settings",type)
+
+st.bar_chart(summary.loc[:,["count"]])
 AreaMarker(df8,m)
 folium_static(m)
 st.write("Data by https://www.npa.go.jp/publications/statistics/koutsuu/opendata/index_opendata.html")
